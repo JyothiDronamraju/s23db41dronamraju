67c5f69d9770cb291aacc661778dd661f109b592
JyothiDronamraju <143140898+JyothiDronamraju@users.noreply.github.com>
2023-10-30 15:49:39 -0500

    added

--- routes/board.js
+++ routes/board.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// Define a route for '/board' (without any query parameters)
+router.get('/', function(req, res, next) {
+  res.render('board', { title: 'Board Display', query: null }); // Pass null for query
+});
+
+// Define a route for '/board/gridbuild' (with query parameters)
 router.get('/gridbuild', function(req, res, next) {
   let query = req.query;
   console.log(`rows ${query.rows}`);
   console.log(`cols ${query.cols}`);
-  res.render('board', { title: 'Board Display', query: query }); // Pass the 'query' object
+  res.render('board', { title: 'Board Display', query: query });
 });
 
 module.exports = router;
