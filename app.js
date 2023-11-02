d0eb12fdb03ed858bec3c9452ac39109fd0986c3
JyothiDronamraju <143140898+JyothiDronamraju@users.noreply.github.com>
2023-11-01 21:38:17 -0500

    trying to sort errors

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apartmentsRouter = require('./routes/apartments');
 var boardRouter = require('./routes/board');
-var chooseRouter = require('./routes/choose');
+
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -21,23 +21,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/apartments', apartmentsRouter);
 app.use('/board', boardRouter);
-app.use('/choose', chooseRouter);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+
+// Handle 404 and errors
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
+  // Handle errors
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
