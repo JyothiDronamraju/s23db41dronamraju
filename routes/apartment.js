0a3a28eebdeee85bc7936ccb13b5914278731c3c
JyothiDronamraju <143140898+JyothiDronamraju@users.noreply.github.com>
2023-11-01 21:07:05 -0500

    added

--- routes/apartment.js
+++ routes/apartment.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-
+/* GET apartment page. */
 router.get('/', function(req, res, next) {
-  res.render('apartment', { title: 'Search Results - Apartments' });
+  res.render('apartment', { title: 'Search Results - Apartment' });
 });
 
 module.exports = router;
