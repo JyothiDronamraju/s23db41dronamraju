b3a3d6736ee3c2533defd4489e6cc3eb2b7149f5
JyothiDronamraju <143140898+JyothiDronamraju@users.noreply.github.com>
2023-11-01 20:47:55 -0500

    made lil changes

--- routes/apartments.js
+++ routes/apartments.js
@@ -1,11 +1,10 @@
-
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-  res.render('nwclass', { title: 'Search Results - Apartments' });
+  res.render('apartments', { title: 'Search Results - Apartments' });
 });
 
 module.exports = router;
