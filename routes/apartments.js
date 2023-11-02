91973163a2cdad196b9b077793d945c0045d6acf
JyothiDronamraju <143140898+JyothiDronamraju@users.noreply.github.com>
2023-11-01 20:21:13 -0500

    added images and files

--- routes/apartments.js
+++ routes/apartments.js
@@ -1,10 +1,11 @@
+
 var express = require('express');
 var router = express.Router();
 
-/* GET apartments page. */
+/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  console.log('Apartments route hit'); // Add this line
-  res.render('apartments', { title: 'Search Results - Apartments' });
+  res.render('nwclass', { title: 'Search Results - Apartments' });
 });
 
 module.exports = router;
