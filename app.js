6fad767b9b1ae5496688e537f78b4f694982028b
JyothiDronamraju <143140898+JyothiDronamraju@users.noreply.github.com>
2023-11-27 13:27:54 -0600

    added

--- app.js
+++ app.js
@@ -4,65 +4,58 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
 require('dotenv').config();
-const connectionString = process.env.MONGO_CON
+const connectionString = process.env.MONGO_CON;
 const mongoose = require('mongoose');
-mongoose.connect(connectionString,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    wtimeoutMS:50000
-  });
-//Get the default connections
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  wtimeoutMS: 50000,
+});
+// Get the default connections
 
 var db = mongoose.connection;
-//Bind connection to error event
-db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+// Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once("open", function () {
-  console.log("Connection to DB succeeded")
+  console.log("Connection to DB succeeded");
 });
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apartmentRouter = require('./routes/apartments');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
-var Apartment = require("./models/apartment");
+var Apartment = require('./models/apartment');
 var resourceRouter = require('./routes/resource');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
- 
-passport.use(new LocalStrategy(
-function(username, password, done) {
-Account.findOne({ username: username })
-.then(function (user){
-if (err) { return done(err); }
-if (!user) {
-return done(null, false, { message: 'Incorrect username.' });
-}
-if (!user.validPassword(password)) {
-return done(null, false, { message: 'Incorrect password.' });
-}
-return done(null, user);
-})
-.catch(function(err){
-return done(err)
-})
-})
-)
+
+
 var app = express();
 
-
-//const Apartment = mongoose.model("Apartment", apartmentSchema);
-
-
-// We can seed the collection if needed onserver start
-async function  recreateDB() {
-  
-  console.log("**********")
+// We can seed the collection if needed on server start
+async function recreateDB() {
+  console.log('**********');
   // Delete everything
- // mongoose.set('bufferTimeoutMS', 50000);
   await Apartment.deleteMany();
   let instance1 = new Apartment({ apartment_name: "Costal Breezes Apartments", location: "Miami, FL", rent: 2200 }); // Corrected rent value
   instance1.save().then(doc => { console.log("First object saved") }).catch(err => {
@@ -98,12 +91,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('express-session')({
-  secret: 'keyboardcat',
+  secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/apartments', apartmentRouter);
 app.use('/', indexRouter);
--- routes/apartments.js
+++ routes/apartments.js
@@ -1,12 +1,13 @@
 var express = require('express');
-const apartment_controlers= require('../controllers/apartment');
+const apartment_controlers = require('../controllers/apartment');
 var router = express.Router();
+const secured = (req,res,next) =>{if (req.user){return next();}res.redirect("/login");}
 
 // GET Apartments
 router.get('/', apartment_controlers.apartment_view_all_Page);
 
 // GET request for one apartment.
-router.get('/:id', apartment_controlers.apartment_detail);
+router.get('/apartments/:id', apartment_controlers.apartment_detail);
 
 /* GET detail apartment page */
 router.get('/detail', apartment_controlers.apartment_view_one_Page);
@@ -15,7 +16,7 @@ router.get('/detail', apartment_controlers.apartment_view_one_Page);
 router.get('/create', apartment_controlers.apartment_create_Page);
 
 /* GET update apartment page */
-router.get('/update', apartment_controlers.apartment_update_Page);
+router.get('/update',secured, apartment_controlers.apartment_update_Page);
 
 /* GET delete apartment page */
 router.get('/delete', apartment_controlers.apartment_delete_Page);
