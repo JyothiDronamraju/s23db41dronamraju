5c931a84e3d995f7dfa70452dd49dbc9c1358fd9
JyothiDronamraju <143140898+JyothiDronamraju@users.noreply.github.com>
2023-11-29 11:31:35 -0600

    added all details

--- models/apartment.js
+++ models/apartment.js
@@ -1,18 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose")
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('apartment', { title: 'Search Results - Apartments' });
+const apartmentSchema = mongoose.Schema({
+  apartment_name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+    // Add your validation for the location attribute here
+    // For example, you can require a minimum length for the location string
+    minlength: [5, 'Location must be at least 5 characters long'],
+    // You can also use regex for more specific validation
+    // match: [/^[a-zA-Z0-9\s,]+$/, 'Invalid characters in location'],
+  },
+  rent: {
+    type: Number,
+    required: true,
+    min: [0, 'Price must be at least 0'],
+    max: [5000, 'Price must be at most 5000'],
+  },
 });
 
-module.exports = router;
-
-
-const mongoose = require("mongoose")
-const apartmentSchema = mongoose.Schema({
-apartment_name: String,
-location: String,
-rent: Number
-})
-module.exports = mongoose.model("Apartment",apartmentSchema)
+module.exports = mongoose.model("Apartment", apartmentSchema);
--- routes/apartments.js
+++ routes/apartments.js
@@ -1,26 +1,32 @@
 var express = require('express');
-const apartment_controlers= require('../controllers/apartment');
+const apartment_controlers = require('../controllers/apartment');
 var router = express.Router();
+
 // A little function to check if we have an authorized user and continue on or
 // redirect to login.
 const secured = (req, res, next) => {
-if (req.user){
-return next();
+  if (req.user) {
+    return next();
+  }
+  res.redirect("/login");
 }
-res.redirect("/login");
-}
+
 // GET Apartments
- router.get('/', apartment_controlers.apartment_view_all_Page );
- //GET request for one apartment.
-router.get('/apartments/:id', apartment_controlers.apartment_detail)
+router.get('/', apartment_controlers.apartment_view_all_Page);
+
+// GET request for one apartment.
+router.get('/apartments/:id', apartment_controlers.apartment_detail);
+
 /* GET detail apartment page */
 router.get('/detail', apartment_controlers.apartment_view_one_Page);
-/* GET create apartment page */
-router.get('/create', apartment_controlers.apartment_create_Page);
-/* GET create update page */
-router.get('/update',secured, apartment_controlers.apartment_update_Page);
-/* GET delete apartment page */
-router.get('/delete', apartment_controlers.apartment_delete_Page);
 
+/* GET create apartment page */
+router.get('/create', secured, apartment_controlers.apartment_create_Page);
+
+/* GET create update page */
+router.get('/update', secured, apartment_controlers.apartment_update_Page);
+
+/* GET delete apartment page */
+router.get('/delete', secured, apartment_controlers.apartment_delete_Page);
 
 module.exports = router;
